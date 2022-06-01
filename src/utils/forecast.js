054679b8caf5d5f81ccfb5671988b53fd849ef4b
unknown <najiib130@hotmail.com>
2022-06-01 18:05:58 +0400

    changes coming from about, picture and humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=1cbc64dbd5e767352f578d8a7cfce7ed&query=' + latitude + ',' + longitude + '&untis=m'
+    const url = 'http://api.weatherstack.com/current?access_key=c10da58d16c7fb52ebd084c6a15f04f6&query=' + latitude + ',' + longitude + '&untis=m'
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather app!', undefined)
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location try again!', undefined)
 
         } else {
-            callback(undefined, body.current.weather_descriptions +'. Its is currently ' + body.current.temperature, ' degrees out. and it feels like ' + body.current.feelslike  + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions +'. Its is currently ' + body.current.temperature + ' degrees out, and it feels like ' + body.current.feelslike  + ' degrees out. the humidity is ' + body.current.humidity)
         }
     })
 }
