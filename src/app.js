e3440f532a34d4e3c553de1b84aa3718c2eefa76
unknown <najiib130@hotmail.com>
2022-06-02 09:49:09 +0400

    Ownership has been changed/nodemon installed localy

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Najib'
+        name: 'Hafido Abdalla'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About me',
-        name: 'Najib'
+        name: 'Hafido Abdalla'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help',(req, res) => {
     res.render('help', {
         helpText: 'this is a very helpfull text',
         title: 'Help',
-        name: 'Najib'
+        name: 'Hafido Abdalla'
     })
 })
 
@@ -91,7 +91,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404',{
         title: '404',
-        name: 'Najib',
+        name: 'Hafido Abdalla',
         errorMessage: 'help article can not be found'
     } )
 })
@@ -99,7 +99,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Najib',
+        name: 'Hafido Abdalla',
         errorMessage: 'page not found'
 
     })
